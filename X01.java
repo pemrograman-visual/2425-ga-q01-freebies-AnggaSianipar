d57faa53f21c5bcfea035d014e0d99c936484874
githubAnggaBrillyantPratamaSianipar <anggasnpr1103@gmail.com>
2024-10-09 16:32:15 +0700

    Pair 09

--- X01.java
+++ X01.java
@@ -1,2 +1,36 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24004 - Silvia Eklesiana Br. Sitorus
+// 12S24032 - Angga Sianipar
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargabuku, total, i, hasilakhir;
+
+        total = 0;
+        hargabuku = Double.parseDouble(input.nextLine());
+        i = hargabuku;
+        while (hargabuku != 0) {
+            total = total + hargabuku;
+            if (i < hargabuku) {
+                i = i;
+            } else {
+                i = hargabuku;
+            }
+            hargabuku = Double.parseDouble(input.nextLine());
+        }
+        if (total >= 100) {
+            hasilakhir = total - i;
+        } else {
+            hasilakhir = hasilakhir;
+        }
+        System.out.println(toFixed(hasilakhir,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
