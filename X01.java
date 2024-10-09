fabd2448aac466a68ccf28146fd3e977951387a2
githubAnggaBrillyantPratamaSianipar <anggasnpr1103@gmail.com>
2024-10-09 16:48:42 +0700

    Pair 09

--- X01.java
+++ X01.java
@@ -4,34 +4,29 @@
 import java.util.*;
 import java.lang.Math;
 
-public class X01{
+public class X01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double hargabuku, total, i, hasilakhir;
+        double hargabuku, total, i, transaksi;
 
         total = 0;
         hargabuku = Double.parseDouble(input.nextLine());
         i = hargabuku;
         while (hargabuku != 0) {
+            total = total + hargabuku;
             if (i < hargabuku) {
                 i = i;
             } else {
                 i = hargabuku;
             }
-            total = total + hargabuku;
             hargabuku = Double.parseDouble(input.nextLine());
         }
         if (total >= 100) {
-            hasilakhir = total - i;
+            transaksi = total - i;
         } else {
-            hasilakhir = hasilakhir;
+            transaksi = transaksi;
         }
-        System.out.println(toFixed(hasilakhir,2));
-    }
-    
-    private static String toFixed(double value, int digits) {
-        return String.format("%." + digits + "f", value);
+        System.out.println(transaksi);
     }
 }
-
