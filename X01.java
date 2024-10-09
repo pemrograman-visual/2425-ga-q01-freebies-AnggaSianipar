8e99fb7eb7e9aa9b517288dded38643a3264d8dc
githubAnggaBrillyantPratamaSianipar <anggasnpr1103@gmail.com>
2024-10-09 16:58:45 +0700

    Pair 09

--- X01.java
+++ X01.java
@@ -30,3 +30,37 @@ public class X01 {
         System.out.println(transaksi);
     }
 }
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargabuku, total, i, hasilakhir, hargaterkecil;
+
+        total = 0;
+        hargabuku = Double.parseDouble(input.nextLine());
+        i = hargabuku;
+        hargaterkecil = 9999;
+        while (hargabuku != 0) {
+            if (i < hargabuku) {
+                i = i;
+            } else {
+                i = hargabuku;
+            }
+            total = total + hargabuku;
+            hargabuku = Double.parseDouble(input.nextLine());
+        }
+        if (total >= 100) {
+            hasilakhir = total - i;
+        } else {
+            hasilakhir = hasilakhir;
+        }
+        System.out.println(toFixed(hasilakhir,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
